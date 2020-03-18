72029ce9e3e146aa01582ac946b3ee46fe2d1ef4
Andy Shapiro <shapiromatron@gmail.com>
2020-03-18 11:23:19 -0400

    build package

--- README.md
+++ README.md
@@ -11,3 +11,30 @@ Run the notebooks in the following order:
 
 3. Visualization.ipynb
     - 2D or 3D plotly visualizations provided.
+
+## Setup
+
+To setup the environment:
+
+```bash
+git clone git@github.com:ricerb/topic-clustering.git
+cd topic-clustering
+
+# create new virtual environment
+python -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+```
+
+To run from the command line:
+
+```bash
+# view help
+refml --help
+
+# run something
+refml do-it
+```
+
+
---
+++ refml/topic_modeling/preprocess.py
@@ -0,0 +1,23 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+
+stop_words = set(stopwords.words("english"))
+ps = PorterStemmer()
+
+
+def stem_and_tokenize(words):
+    tokens = word_tokenize(words)
+
+    stemmed_words = []
+    for word in tokens:
+        stemmed_words.append(ps.stem(word))
+
+    stopword_filtered = [w for w in stemmed_words if w not in stop_words]
+
+    # remove digits and punctuation
+    pattern = re.compile(r"[a-zA-Z]+")
+    cleaned = [x for x in stopword_filtered if pattern.search(x)]
+
+    return cleaned
