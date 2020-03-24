c3c0221988b5d7d386168c9c2b8c79d67e1c6db6
Rice <rice.richard@epa.gov>
2020-03-24 13:26:33 -0400

    run streamlit app from command line in package

--- refml/cli.py
+++ refml/cli.py
@@ -10,3 +10,7 @@ def cli():
 def do_it():
     """Do something..."""
     click.echo("You did it!")
+    import os
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'apps/refml_app.py')
+    os.system('streamlit run ' + filename)
